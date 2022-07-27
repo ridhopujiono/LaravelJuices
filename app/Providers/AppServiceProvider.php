593f254f2513e78d9ce9e82356ada7aca761a62b
ridhopujiono <ridho@smktjp.sch.id>
2022-07-27 13:37:18 +0700

    change env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+
+        config(['app.locale' => 'id']);
+        \Carbon\Carbon::setLocale('id');
+    }
     }
 }
